e905beaa3f12f4d099be7b87db65bbcac8898635
Li Swafford <liswafford@yahoo.com>
2019-01-07 15:31:18 -0600

    follow course

---
+++ Starting Code/app.js
@@ -0,0 +1,13 @@
+// Put this in the script section in JSFiddle
+// In a local setup, you need to merge this and the index.html file into one file
+new Vue({
+	el: '#app',
+	data: {
+		show: true,
+    ingredients: ['meat', 'fruit', 'cookies'],
+    persons: [
+    	{name: 'Max', age: 27, color: 'red'},
+      {name: 'anna', age: 'unknown', color: 'blue'}
+    ]
+	}
+})
