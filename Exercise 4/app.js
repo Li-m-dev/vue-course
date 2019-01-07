0522626d15a5a4ef795ec1fa913af7193e4d214b
Li Swafford <liswafford@yahoo.com>
2019-01-06 18:49:46 -0600

    v-model is not working

--- Exercise 4/app.js
+++ Exercise 4/app.js
@@ -1,11 +1,22 @@
 new Vue({
   el: '#exercise',
   data: {
+    effectClass: {
+      highlight: false,
+      shrink: true
+    },
+    userClass: '',
 
   },
+
   methods: {
     startEffect: function() {
-    
+      let vm = this;
+      setInterval(function() {
+        console.log('hit');
+        vm.effectClass.highlight = !vm.effectClass.highlight;
+        vm.effectClass.shrink = !vm.effectClass.shrink;
+      }, 1000)
     }
   }
 });
