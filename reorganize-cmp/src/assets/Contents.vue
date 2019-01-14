2fdb1600a5bbb3387ad0053d8b31d72509419e5d
Li Swafford <liswafford@yahoo.com>
2019-01-14 16:14:56 -0600

    restructure

---
+++ reorganize-cmp/src/assets/Contents.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="row">
+    <ServerList/>
+    <ServerStatus/>
+</div>
+</template>
+<script>
+import ServerList from './ServerList.vue';
+import ServerStatus from './ServerStatus.vue'
+export default {
+  components: {
+    ServerList,
+    ServerStatus
+  }
+}
+</script>
+
---
+++ reorganize-cmp/src/assets/Home.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="container">
+        <Header/>
+        <hr>
+        <Contents/>
+        <hr>
+        <Footer/>
+    </div>
+</template>
+
+<script>
+import Header from './Header.vue';
+import Contents from './Contents.vue';
+import Footer from './Footer.vue';
+export default {
+    components: {
+        Header,
+        Contents,
+        Footer
+    }
+}
+</script>
+
