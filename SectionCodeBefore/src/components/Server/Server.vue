b45a8d608b19338cbeb4af5d4cc09efb87e065d9
Li Swafford <liswafford@yahoo.com>
2019-01-15 15:30:01 -0600

    props, ,eventBus

---
+++ SectionCodeBefore/src/components/Server/Server.vue
@@ -0,0 +1,27 @@
+<template>
+
+  <li 
+  class="list-group-item"
+  style="cusor: pointer"
+  @click="serverSelected"
+  >
+      Server #{{ server.id }}
+  </li>
+
+</template>
+
+
+<script>
+
+import {serverBus} from '../../main.js';
+
+export default {
+  props: ['server'],
+  methods: {
+    serverSelected() {
+      // console.log('test');
+      serverBus.$emit('serverSelected', this.server)
+    }
+  }
+}
+</script>
--- SectionCodeBefore/src/components/Server/ServerDetails.vue
+++ SectionCodeBefore/src/components/Server/ServerDetails.vue
@@ -1,11 +1,34 @@
 <template>
     <div class="col-xs-12 col-sm-6">
-        <p>Server Details are currently not updated</p>
+        <p v-if="!server">Please select a Server</p>
+        <p v-else>Server# {{ server.id }} selected, Status: {{ server.status}}</p>
+        <hr>
+        <button @click="changeStatus">Update Status</button>
     </div>
 
 </template>
 
 <script>
+
+    import {serverBus} from '../../main.js';
+
+    export default{
+        data() {
+            return {
+                server: null
+            }
+        },
+        methods: {
+            changeStatus() {
+                this.server.status = 'Normal';
+            }
+        },
+        created() {
+            serverBus.$on('serverSelected', (server) => {
+                this.server = server;
+            } )
+        }
+    }
 </script>
 
 <style scoped>
