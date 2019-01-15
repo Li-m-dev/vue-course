e04205837f89e6ded18d757ea2e04e19b7e1bf3f
Li Swafford <liswafford@yahoo.com>
2019-01-15 16:37:51 -0600

    dynamic component

--- advance-cmp-usage/src/App.vue
+++ advance-cmp-usage/src/App.vue
@@ -2,18 +2,43 @@
     <div class="container">
         <div class="row">
             <div class="col-xs-12">
-               
+                <button @click= "selectedComponent = 'Quote'">Quote</button>
+                <button @click= "selectedComponent = 'Author'">Author</button>
+                <button @click= "selectedComponent = 'New'">New</button>
+                <hr>
+                <p>{{ selectedComponent }}</p>
+                <!-- <Quote>
+                <h2 slot="title">{{ quoteTitle }}</h2>
+                <p slot="content">This is wonderful</p>
+                </Quote> -->
+                <component :is="selectedComponent">
+                    <p>Default Content</p>
+                </component>
+
             </div>
         </div>
     </div>
 </template>
 
 <script>
+    import Quote from './components/Quote.vue';
+    import Author from './components/Author.vue';
+    import New from './components/New.vue';
 
     export default {
-        
+        data() {
+            return {
+                quoteTitle: 'The Quote1',
+                selectedComponent: 'Quote'
+            }
+        },
+        components: {
+            Quote,
+            Author,
+            New
+        }
     }
 </script>
 
-<style>
+<style scoped>
 </style>
