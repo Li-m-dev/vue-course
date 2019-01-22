8942b7c33da58e36f64e85f669e90d169b9f8ffa
Li Swafford <liswafford@yahoo.com>
2019-01-22 15:35:25 -0600

    v-model.lazy

--- formAssignment/src/App.vue
+++ formAssignment/src/App.vue
@@ -9,6 +9,51 @@
                     <!-- Mail -->
                     <!-- Password -->
                     <!-- Store Data? Yes/No -->
+                    <div class="form-group">
+                        <label for="name">Full name</label>
+                        <input
+                            type="text"
+                            id="name"
+                            class="form-control"
+                            v-model="userInfo.name"
+                            >
+                    </div>
+                    <div class="form-group">
+                        <label for="email">Mail</label>
+                        <input
+                            type="text"
+                            id="email"
+                            class="form-control"
+                            v-model="userInfo.email"
+                            >
+                            <!-- <p>{{ userInfo.email }}</p> -->
+                    </div>
+                    <div class="form-group">
+                        <label for="password">Password</label>
+                        <input 
+                        type="password"
+                        id="password"
+                        class="form-control"
+                        v-model.lazy="userInfo.password">
+                        <!-- <p>{{userInfo.password}}</p> -->
+                    </div>
+                    <div class="form-group">
+                        <label for="storeData">Store Data?</label>
+                        <label for="yes">
+                            <input
+                                type="radio"
+                                id="yes"
+                                value="Yes"
+                                v-model="storeData"> Yes
+                        </label>
+                        <label for="no">
+                            <input
+                                type="radio"
+                                id="no"
+                                value="No"
+                                v-model="storeData"> No
+                        </label>
+                    </div>
 
                     <!-- Exercise 2 -->
                     <!-- Only display the Form if it has NOT been submitted -->
@@ -19,19 +64,29 @@
                     <!-- which still holds the First Name and Last Name Input Field -->
                 </div>
             </div>
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                    <button
+                        class="btn btn-primary"
+                        @click.prevent="submitted"
+                        >Submit
+                    </button>
+                </div>
+            </div>
         </form>
         <hr>
-        <div class="row">
+        <div class="row"
+        v-if="isSubmitted">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <div class="panel panel-default">
                     <div class="panel-heading">
                         <h4>Your Data</h4>
                     </div>
                     <div class="panel-body">
-                        <p>Full Name: </p>
-                        <p>Mail: </p>
-                        <p>Password: </p>
-                        <p>Store in Database?: </p>
+                        <p>Full Name: {{ userInfo.name }}</p>
+                        <p>Mail: {{ userInfo.email}}</p>
+                        <p>Password: {{ userInfo.password }}</p>
+                        <p>Store in Database?: {{ storeData }}</p>
                     </div>
                 </div>
             </div>
@@ -41,6 +96,22 @@
 
 <script>
     export default {
+        data() {
+            return {
+                userInfo:{
+                    name : "",
+                    email : "",
+                    password : ""
+                },
+                isSubmitted: false,
+                storeData: "yes"
+            }
+        },
+        methods: {
+            submitted(){
+                this.isSubmitted = true;
+            }
+        }
     }
 </script>
 
